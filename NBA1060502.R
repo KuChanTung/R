3bb41ad0a3a13462e869e9a7e8d5afa0ec6c79db
KuChanTung <kuchantung@gmail.com>
2017-05-03 01:56:25 +0800

    Finish the mid project and build R code in RStudio

--- NBA1060502.R
+++ NBA1060502.R
@@ -1,6 +1,6 @@
 
-#讀取檔案NBA2016-17球季全聯盟30隊各項攻防均數
-NBA1617Analysis <- read.csv("NBA1617TeamPerGameStats.csv",header=T,sep = ",")
+#讀取檔案NBA2016-17球季全聯盟30隊各項攻擊均數
+NBA1617Analysis <- read.csv("NBA1617TeamPerGameStatsAll.csv",header=T,sep = ",")
 attach(NBA1617Analysis)
 #籃球是以單位時間內得分多寡為勝負的比賽，先利用球隊場均得分進行敘述統計演練
 NBATEAMPTS <- NBA1617Analysis$PTS
@@ -39,7 +39,7 @@ round(prop.table(xtabs(PTS~Conference+Area,data=NBA1617Analysis)),2)
 round(prop.table(xtabs(PTS~Conference+Area,data=NBA1617Analysis),margin = 1),2)
 #各直行的邊際比例
 round(prop.table(xtabs(PTS~Conference+Area,data=NBA1617Analysis),margin =2),2)
-#寫一個function找出全聯盟均分統計數據
+#找出全聯盟均分統計數據
 #x=NBATEAMPTS
 my.desc=function(x)
 {
@@ -63,7 +63,6 @@ kurtosis(NBATEAMPTS)
 #畫出枝葉圖
 stem(NBATEAMPTS)
 
-
 #常態性檢定 P值0.2283 > 0.05 
 shapiro.test(NBATEAMPTS)
 #判定機率分配---->logistic
@@ -74,27 +73,60 @@ rapidFitFun(NBATEAMPTS[NBA1617Analysis$Conference=="Eastern"])
 #查看西區球隊的場均得分屬於何種機率分配--->logistic
 rapidFitFun(NBATEAMPTS[NBA1617Analysis$Conference=="Western"])
 
-#Logistic線性回歸演練 
+#Logistic線性回歸
 #晉級Promotion與場均分PTS
-modellog<- glm(formula = Promotion~NBATEAMPTS,data = NBA1617Analysis,family = binomial(link = "logit"))
-summary(modellog)
-#以場均得分最低預測在聯盟例賽獲勝機率為0.2049738
-predict.glm(modellog,type="response",newdata = data.frame(NBATEAMPTS=97.9))
-#以場均得分最高預測在聯盟例賽獲勝機率為0.9028666
-predict.glm(modellog,type="response",newdata = data.frame(NBATEAMPTS=115.9))
-#場均得分多的0.1992 可以增加1.220426勝率
-exp(0.1992)
-
+#聯盟東區球隊
+Eastern <- read.csv("EasternTeamPerGameStats.csv",header=T,sep = ",")
+attach(Eastern)
+ETEAMPTS <- Eastern$PTS
+Emodellog<- glm(formula = Promotion~ETEAMPTS,data = Eastern,family = binomial(link = "logit"))
+summary(Emodellog)
+#以場均得分最低預測在聯盟例賽獲勝機率為0.1430553
+predict.glm(Emodellog,type="response",newdata = data.frame(ETEAMPTS=101.1))
+#以場均得分最高預測在聯盟例賽獲勝機率為0.9673395
+predict.glm(Emodellog,type="response",newdata = data.frame(ETEAMPTS=110.3))
+Emodellog$coefficients
+#場均得分多的0.5628832 可以增加1.755727勝率
+exp(0.5628832)
 #設定分區及分組為類別變數
-areaf <- factor(Area)
-Conferencef <- factor(Conference)
+Eareaf <- factor(Eastern$Area)
+EPromotionf <- factor(Eastern$Promotion)
 #驗證分區及分組的變數設定正確
-is.factor(areaf)
-is.factor(Conferencef)
-#設定以西區太平洋組（3）為準，針對分區與分組進行Logistic迴歸分析
-contrasts(areaf) <- contr.treatment(6,base = 3)
-model2 <- glm(formula = Promotion~areaf+Conferencef,data=NBA1617Analysis,family = binomial(link = "logit"))
-summary(model2)
-#分組球隊在不同分區晉級可能性預測
-classtab <- table(Area,Promotion)
+is.factor(Eareaf)
+is.factor(EPromotionf)
+#進行Logistic迴歸分析
+Emodel2 <- glm(formula = Eastern$Promotion~Eareaf+EPromotionf,data=Eastern,family = binomial(link = "logit"))
+summary(Emodel2)
+epiDisplay::logistic.display(Emodel2)
+#分組球隊在分區晉級可能性預測
+classtab <- table(Eastern$Area,Eastern$Promotion)
 prop.table(classtab,1)
+
+
+#聯盟西區球隊
+Western <- read.csv("WesternTeamPerGameStats.csv",header=T,sep = ",")
+attach(Western)
+WTEAMPTS <- Western$PTS
+Wmodellog<- glm(formula = Promotion~WTEAMPTS,data = Western,family = binomial(link = "logit"))
+summary(Wmodellog)
+#以場均得分最低預測在聯盟例賽獲勝機率為0.301573
+predict.glm(Wmodellog,type="response",newdata = data.frame(WTEAMPTS=97.9))
+#以場均得分最高預測在聯盟例賽獲勝機率為0.7797157
+predict.glm(Wmodellog,type="response",newdata = data.frame(WTEAMPTS=115.9))
+Wmodellog$coefficients
+#場均得分多的0.1168794 可以增加1.123984勝率
+exp(0.1168794)
+#設定分區及分組為類別變數
+Wareaf <- factor(Western$Area)
+WPromotionf <- factor(Western$Promotion)
+#驗證分區及分組的變數設定正確
+is.factor(Wareaf)
+is.factor(WPromotionf)
+#進行Logistic迴歸分析
+Wmodel2 <- glm(formula = Western$Promotion~Wareaf+WPromotionf,data=Western,family = binomial(link = "logit"))
+summary(model2)
+epiDisplay::logistic.display(Wmodel2)
+#分組球隊在分區晉級可能性預測
+classtab <- table(Western$Area,Western$Promotion)
+prop.table(classtab,1)
+

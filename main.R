856e75e31f1fa286f73efbc263e0fb34457b6a46
KuChanTung <kuchantung@gmail.com>
2017-05-18 19:40:43 +0800

    Add crawler Code

---
+++ main.R
@@ -0,0 +1,72 @@
+source("crawler_CIA_CREST.R")
+
+##################################################################################
+# Example for 'Kennedy'
+# Download CIA files of 'Kennedy' in top 10 pages  
+
+#=== 1. Give a query to get basic information ===#
+basic.info.query.CIA_CREST(query = "Kennedy") 
+
+#=== 2. Parse results according to given query and pages ===#
+your.query = 'Kennedy'
+page.nums = c(0:9)    # top 10 pages (note that 0 is the 1st page on the web)
+# return a parse.table
+parse.table = parsing.pages.CIA_CREST(your.query, page.nums)
+
+#=== 3. Auto-download files according to the parse.table ===#
+# download files according to the parse.table from parsing.pages.CIA_CREST()
+# and return a reference.table
+reference.table = download.doc.CIA_CREST(parse.table)
+
+
+##################################################################################
+# Example for 'secret letter'
+# Download CIA files of 'secret letter' in 0,2,4 pages  
+# (note that 0 is the 1st page on the web)
+
+#=== 1. Give a query to get basic information ===#
+basic.info.query.CIA_CREST(query = "secret letter") 
+
+#=== 2. Parse results according to given query and pages ===#
+your.query = 'secret letter'
+page.nums = c(0,2,4)    
+# return a parse.table
+parse.table = parsing.pages.CIA_CREST(your.query, page.nums)
+
+#=== 3. Auto-download files according to the parse.table ===#
+# download files according to the parse.table from parsing.pages.CIA_CREST()
+# and return a reference.table
+reference.table = download.doc.CIA_CREST(parse.table)
+
+
+
+##################################################################################
+# Example for 'Obama'
+# Download CIA files of 'Obama' in 0~1 pages  
+# (note that 0 is the 1st page on the web)
+#=== 1. Give a query to get basic information ===#
+basic.info.query.CIA_CREST(query = "Obama") 
+
+#=== 2. Parse results according to given query and pages ===#
+your.query = 'Obama'
+page.nums = c(0,1)    # 0~1 pages (note that 0 is the 1st page on the web)
+parse.table = parsing.pages.CIA_CREST(your.query, page.nums)
+
+#=== 3. Auto-download files according to the parse.table ===#
+reference.table = download.doc.CIA_CREST(parse.table)
+
+##################################################################################
+# Example for 'UFO'
+# Download the top 10 CIA files of 'UFO' in 0 pages 
+# (note that 0 is the 1st page on the web)
+
+#=== 1. Give a query to get basic information ===#
+basic.info.query.CIA_CREST(query = "UFO") 
+
+#=== 2. Parse results according to given query and pages ===#
+your.query = 'UFO'
+page.nums = c(0)    #  (note that 0 is the 1st page on the web)
+parse.table = parsing.pages.CIA_CREST(your.query, page.nums)
+
+#=== 3. Auto-download files according to the parse.table ===#
+reference.table = download.doc.CIA_CREST(parse.table[1:10, ])

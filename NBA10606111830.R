92b0ea845466b56386507e0b1aebdae914946093
KuChanTung <kuchantung@gmail.com>
2017-09-24 16:24:06 +0800

    Add Code

--- NBA10606111830.R
+++ NBA10606111830.R
@@ -209,7 +209,9 @@ legend('topright', colnames(NBA.newrf$err.rate), col=1:3, fill=1:3)
 print(NBA.newrf.Opt)
 
 #第2次預測2017年度總冠軍
+#NBA.LC.newprediction <- predict(NBA.newrf.Opt, NBA.newrf.test,type='prob')
 NBA.LC.newprediction <- predict(NBA.newrf.Opt, NBA.newrf.test,type='prob')
+
 #solution <- data.frame(OrininalPrediction = NBA.rf.test$Final, RF.Prediction = NBA.LC.newprediction)
 newsolution <- data.frame(RF.Prediction = NBA.LC.newprediction)
 #將預測結果寫入檔案
